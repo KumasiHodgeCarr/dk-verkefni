64ecf617759b9dc00d0398c2295eb740e690dded
Kumasi Máni Hodge-Carr <Kumasimani19@gmail.com>
2026-03-07 20:22:49 +0000

    showcasing data start, show data

--- app/page.tsx
+++ app/page.tsx
@@ -7,4 +7,27 @@ export default async function HomePage() {
     fetchRedditPost(REDDIT_POSTS["countries-bombed-since-1945"].url),
     fetchRedditPost(REDDIT_POSTS["us-attacks-21st-century"].url),
   ]);
+
+  const posts = [
+    { thread: thread1, slug: "countries-bombed-since-1945" },
+    { thread: thread2, slug: "us-attacks-21st-century" },
+  ];
+
+  return (
+    <main>
+      <h1 className="">Conflict Lens</h1>
+
+      <div>
+        {posts.map(({ thread, slug }) => (
+          <div key={slug} className="">
+            <p>{thread.post.subreddit}</p>
+            <h2> {thread.post.title}</h2>
+            <p>
+              {thread.post.score} ~ {thread.post.numComments} Comments
+            </p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
